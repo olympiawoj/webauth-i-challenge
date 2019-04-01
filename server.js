f657df52a2ab801761aee1104a035de62f2143fe
olympia <owojcik7@gmail.com>
2019-04-01 16:57:50 -0400

    Add bcrypt library

--- server.js
+++ server.js
@@ -1,14 +1,21 @@
 const express = require("express");
+const helmet = require("helmet");
+const cors = require("cors");
 const server = express();
 
 //import routers here
 
+server.use(helmet());
 server.use(express.json());
-
+server.use(cors());
 //use routers here
 
 server.get("/", (req, res) => {
   res.send("server is workin aiiight");
+});
+
+server.post("/api/register", (req, res) => {
+  let user = req.body;
 });
 
 module.exports = server;
