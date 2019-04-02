c7ed573018a9186059c53e5d401762113ae68a25
olympia <owojcik7@gmail.com>
2019-04-02 17:03:19 -0400

    Refactor code to include auth folder and auth-router

---
+++ auth/auth-router.js
@@ -0,0 +1,48 @@
+const router = require("express").Router();
+const bcrypt = require("bcryptjs");
+const db = require("../data/dbConfig.js");
+
+//register new username and password
+router.post("/register", (req, res) => {
+  let user = req.body;
+  console.log(user);
+  // //hash the password
+  const hash = bcrypt.hashSync(user.password, 4);
+  user.password = hash;
+  //post user
+  db("users")
+    .insert(user)
+    .then(ids => {
+      console.log(ids);
+      const id = ids[0];
+      db("users")
+        .where({ user_id: id })
+        .then(ids => res.status(201).json(ids));
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+});
+
+//login using user name and password
+router.post("/login", (req, res) => {
+  let { name, password } = req.body;
+  console.log(name, password);
+  db("users")
+    .where({ name })
+    .first()
+    .then(user => {
+      //check password against db
+      console.log(user);
+      if (user && bcrypt.compareSync(password, user.password)) {
+        res.status(200).json({ message: `Welcome ${user.name}` });
+      } else {
+        res.status(401).json({ message: "Invalid credentials" });
+      }
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -2,18 +2,20 @@ const express = require("express");
 const helmet = require("helmet");
 const cors = require("cors");
 const bcrypt = require("bcryptjs");
-
+const session = require("express-session");
 const db = require("./data/dbConfig.js");
 
 const server = express();
 
 //import routers here
+const authRouter = require("./auth/auth-router.js");
 
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
 
 //use routers here
+server.use("/api/auth", authRouter);
 
 server.get("/", (req, res) => {
   res.send("server is workin aiiight");
@@ -62,48 +64,5 @@ function only(name) {
     }
   };
 }
-
-//register new username and password
-server.post("/api/register", (req, res) => {
-  let user = req.body;
-  console.log(user);
-  // //hash the password
-  const hash = bcrypt.hashSync(user.password, 4);
-  user.password = hash;
-  //post user
-  db("users")
-    .insert(user)
-    .then(ids => {
-      console.log(ids);
-      const id = ids[0];
-      db("users")
-        .where({ user_id: id })
-        .then(ids => res.status(201).json(ids));
-    })
-    .catch(error => {
-      res.status(500).json(error);
-    });
-});
-
-//login using user name and password
-server.post("/api/login", (req, res) => {
-  let { name, password } = req.body;
-  console.log(name, password);
-  db("users")
-    .where({ name })
-    .first()
-    .then(user => {
-      //check password against db
-      console.log(user);
-      if (user && bcrypt.compareSync(password, user.password)) {
-        res.status(200).json({ message: `Welcome ${user.name}` });
-      } else {
-        res.status(401).json({ message: "Invalid credentials" });
-      }
-    })
-    .catch(error => {
-      res.status(500).json(error);
-    });
-});
 
 module.exports = server;
