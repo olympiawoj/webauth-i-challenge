97dc8c20a2eddfb0f50281976dddfa21495a49ce
olympia <owojcik7@gmail.com>
2019-04-02 11:26:05 -0400

    Add ability to check credentials on login

--- server.js
+++ server.js
@@ -48,4 +48,25 @@ server.post("/api/register", (req, res) => {
     });
 });
 
+//login using user name and password
+server.post("/api/login", (req, res) => {
+  let { name, password } = req.body;
+  console.log(name, password);
+  db("users")
+    .where({ name })
+    .first()
+    .then(user => {
+      //check password against db
+      console.log(user);
+      if (user && bcrypt.compareSync(password, user.password)) {
+        res.status(200).json({ message: `Welcome ${user.name}` });
+      } else {
+        res.status(401).json({ message: "Invalid credentials" });
+      }
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    });
+});
+
 module.exports = server;
