c979a34ce62a63861e897607a13363a26c6f7710
olympia <owojcik7@gmail.com>
2019-04-02 17:10:29 -0400

    Refactor code to pull out restricted middleware

---
+++ auth/restricted-middleware.js
@@ -0,0 +1,26 @@
+const bcrypt = require("bcryptjs");
+const db = require("../data/dbConfig.js");
+
+function restricted(req, res, next) {
+  const { name, password } = req.headers; //grabs
+
+  if (name && password) {
+    db("users")
+      .where({ name })
+      .first()
+      .then(user => {
+        if (user && bcrypt.compareSync(password, user.password)) {
+          next();
+        } else {
+          res.status(401).json({ message: "You cannot pass!!" });
+        }
+      })
+      .catch(error => {
+        res.status(500).json(error);
+      });
+  } else {
+    res.status(401).json({ message: "Please provide credentials" });
+  }
+}
+
+module.exports = restricted;
--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 const bcrypt = require("bcryptjs");
 const session = require("express-session");
 const db = require("./data/dbConfig.js");
+const restricted = require("./auth/restricted-middleware.js");
 
 const server = express();
 
@@ -32,28 +33,6 @@ server.get("/api/users", restricted, only("olympia"), (req, res) => {
     })
     .catch(err => res.status(500).json(error));
 });
-
-function restricted(req, res, next) {
-  const { name, password } = req.headers; //grabs
-
-  if (name && password) {
-    db("users")
-      .where({ name })
-      .first()
-      .then(user => {
-        if (user && bcrypt.compareSync(password, user.password)) {
-          next();
-        } else {
-          res.status(401).json({ message: "You cannot pass!!" });
-        }
-      })
-      .catch(error => {
-        res.status(500).json(error);
-      });
-  } else {
-    res.status(401).json({ message: "Please provide credentials" });
-  }
-}
 
 function only(name) {
   return function(req, res, next) {
