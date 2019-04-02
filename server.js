1aadcc441a7a73b4aeba296fe9df80038aa9998f
olympia <owojcik7@gmail.com>
2019-04-02 16:33:36 -0400

    Use authentication to protected users endpoints, restricting to users that provide credentials

--- server.js
+++ server.js
@@ -20,7 +20,10 @@ server.get("/", (req, res) => {
 });
 
 //get all users
-server.get("/api/users", (req, res) => {
+//restrict access to endpoint
+// | GET    | /api/users    | If the user is logged in, respond with an array of all the users contained in the database. If the user is not logged in repond with the correct status code and the message: 'You shall not pass!'.
+
+server.get("/api/users", restricted, (req, res) => {
   db("users")
     .then(users => {
       res.status(200).json(users);
@@ -28,9 +31,32 @@ server.get("/api/users", (req, res) => {
     .catch(err => res.status(500).json(error));
 });
 
+function restricted(req, res, next) {
+  const { name, password } = req.headers; //grabs
+
+  if (name && password) {
+    db("users")
+      .where({ name })
+      .first()
+      .then(user => {
+        if (user && bcrypt.compareSync(password, user.password)) {
+          next();
+        } else {
+          res.status(401).json({ message: "You cannot pass!!" });
+        }
+      })
+      .catch(error => {
+        res.status(500).json(error);
+      });
+  } else {
+    res.status(401).json({ message: "Please provide credentials" });
+  }
+}
+
 //register new username and password
 server.post("/api/register", (req, res) => {
   let user = req.body;
+  console.log(user);
   // //hash the password
   const hash = bcrypt.hashSync(user.password, 4);
   user.password = hash;
@@ -38,6 +64,7 @@ server.post("/api/register", (req, res) => {
   db("users")
     .insert(user)
     .then(ids => {
+      console.log(ids);
       const id = ids[0];
       db("users")
         .where({ user_id: id })
