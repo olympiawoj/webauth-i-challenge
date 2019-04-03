e27bfcdeb0c132dfcc13b8ea3393d92127e9c485
olympia <owojcik7@gmail.com>
2019-04-03 09:28:42 -0400

    Refact get users code to use await/async and then/catch

--- users/users-router.js
+++ users/users-router.js
@@ -7,18 +7,31 @@ const restricted = require("../auth/restricted-middleware.js");
 //restrict access to endpoint
 // | GET    | /api/users    | If the user is logged in, respond with an array of all the users contained in the database. If the user is not logged in repond with the correct status code and the message: 'You shall not pass!'.
 
-router.get("/users", restricted, (req, res) => {
-  Users.find()
-    .then(users => {
-      console.log(users);
-      res.status(200).json(users);
-    })
-    .catch(err => {
-      console.log(err);
-      res
-        .status(500)
-        .json({ message: "Invalid credentials while trying to see users" });
-    });
+//GET USERS: THEN & CATCH
+// router.get("/users", restricted, (req, res) => {
+//   Users.find()
+//     .then(users => {
+//       console.log(users);
+//       res.status(200).json(users);
+//     })
+//     .catch(err => {
+//       console.log(err);
+//       res
+//         .status(500)
+//         .json({ message: "Invalid credentials while trying to see users" });
+//     });
+// });
+
+//GET USERS: AWAIT & ASYNC
+router.get("/users", restricted, async (req, res) => {
+  try {
+    const users = await Users.find();
+    res.status(200).json(users);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Invalid credentials while trying to see users" });
+  }
 });
 
 function only(name) {
